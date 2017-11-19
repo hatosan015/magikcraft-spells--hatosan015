94a46b65ccf153e81dc40f3c6e60a6130c3e0a16
hatosan015 <harutox2@katane.raindrop.jp>
2017-11-19 05:31:05 +0000

    Update feedme.js

--- feedme.js
+++ feedme.js
@@ -2,4 +2,18 @@ const magik = magikcraft.io;
 
 function feedme() {
 	magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    magik.satio();
+    
 }
